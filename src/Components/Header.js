a259363eb727d1d9589cd7cbdbdf3754a9763dd7
Pratham Saxena <er.prathamsaxena@gmail.com>
2023-08-14 15:30:06 +0530

    Header Improved

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,37 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import '../Header.css'
 import { useContext } from 'react';
 import { TASKCONTEXT } from '../Context/ContextAPI';
 const Header = () => {
   const { taskList } = useContext(TASKCONTEXT);
-  const [count,setCount]=useState(0);
-  useEffect(()=>{
-    taskList.forEach(element => {
-      if(!element.completed)
-      {
-        setCount(count+1);
-      }
-    });
-  },[taskList])
   return (
-       <header id="navbar">
-        <nav className="navbar-container container">
-          <a href="/" className="home-link">
-        <div className="navbar-logo" />
-     Total Task :{taskList.length}
-    </a>
-    <button type="button" id="navbar-toggle" aria-controls="navbar-menu" aria-label="Toggle menu" aria-expanded="false">
-      <span className="icon-bar" />
-      <span className="icon-bar" />
-      <span className="icon-bar" />
-    </button>
-    <div id="navbar-menu" aria-labelledby="navbar-toggle">
-      <ul className="navbar-links">
-        <li className="navbar-item"><p className="navbar-link" >Task Left: {count} </p></li>
-       
-      </ul>
-    </div>
-  </nav>
+       <header id="navbar" className="navbar-container container">
+           <ul className="navbar-links" >
+            <li className="navbar-item" ><p className="navbar-link" style={{justifyContent:"flex-end"}} >Task Left: {taskList.length} </p></li>
+          </ul>
 </header>
 
    
