506740e5878277a5b7ac980d787a2c5bab37ab89
Pratham Saxena <er.prathamsaxena@gmail.com>
2023-08-14 12:53:04 +0530

    Task Left Functionality Enabled!

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../Header.css'
 import { useContext } from 'react';
 import { TASKCONTEXT } from '../Context/ContextAPI';
-
 const Header = () => {
   const { taskList } = useContext(TASKCONTEXT);
+  const [count,setCount]=useState(0);
+  useEffect(()=>{
+    taskList.forEach(element => {
+      if(!element.todo_status)
+      {
+        setCount(count+1);
+      }
+    });
+  },[taskList])
   return (
-    
        <header id="navbar">
         <nav className="navbar-container container">
           <a href="/" className="home-link">
@@ -20,7 +27,7 @@ const Header = () => {
     </button>
     <div id="navbar-menu" aria-labelledby="navbar-toggle">
       <ul className="navbar-links">
-        <li className="navbar-item"><p className="navbar-link" >Task Left(7)</p></li>
+        <li className="navbar-item"><p className="navbar-link" >Task Left: {count} </p></li>
        
       </ul>
     </div>
